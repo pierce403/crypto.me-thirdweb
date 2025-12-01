8c26917def7365f8f290e8208d70a5853b2b0513
Dean Pierce <pierce403@gmail.com>
2025-12-01 09:55:51 -0800

    Fix Prisma type error and remove debug endpoint

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,14 +2,17 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-globalForPrisma.prisma ??
+const prisma =
+  globalForPrisma.prisma ??
   new PrismaClient({
     datasources: {
       db: {
-        url: process.env.POSTGRES_PRISMA_URL || process.env.DATABASE_URL,
+        url: process.env.POSTGRES_PRISMA_URL || process.env.DATABASE_URL || '',
       },
     },
     // optional: log: ['query', 'error', 'warn']
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
